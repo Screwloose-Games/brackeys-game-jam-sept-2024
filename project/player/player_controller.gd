a0aded7b61d812b9d66a1c664903bc72bdfcd3d4
Simpathey <62914757+Simpathey@users.noreply.github.com>
2024-09-09 15:11:46 -0700

    States added to player controller

--- project/player/player_controller.gd
+++ project/player/player_controller.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+enum PlayerState {land,water,breached,}
 
 @export  var base_jump_velocity = -10.0
 @export var damping_factor = 0.6
@@ -9,36 +10,49 @@ extends CharacterBody2D
 var jumping = false
 var current_trail: JumpTrail
 var landed = false
+@export var player_state: PlayerState = PlayerState.land
 
 func _ready():
   jump_arrow.jump.connect(_on_jump)
 
 func _on_jump(angle: float, power: float):
-    if is_on_floor():
-        var direction = Vector2(cos(angle), sin(angle))
-        
-        var v_x = direction.x * power * base_jump_velocity
-        velocity.x = v_x
+  match player_state:
+    PlayerState.land:
+      if is_on_floor():
+          var direction = Vector2(cos(angle), sin(angle))
+          
+          var v_x = direction.x * power * base_jump_velocity
+          velocity.x = v_x
 
-        var v_y = direction.y * power * base_jump_velocity
-        velocity.y = v_y
-        jumping = true
-        $frog_hop_audio._play_hop()
-        make_trail()
+          var v_y = direction.y * power * base_jump_velocity
+          velocity.y = v_y
+          jumping = true
+          $frog_hop_audio._play_hop()
+          make_trail()
+    PlayerState.water:
+      pass
+    PlayerState.breached:
+      pass
         
 func _physics_process(delta: float) -> void:
-  if not is_on_floor():
-    landed = false
-    jumping = false
-    velocity += get_gravity() * delta
-  else:
-    if not landed:
-      $frog_hop_audio._play_landing(false)
-      landed = true
-    
-    velocity.x *= damping_factor
-    if abs(velocity.x) < 0.1:
-        velocity.x = 0
+  match player_state:
+    PlayerState.land:
+      if not is_on_floor():
+        landed = false
+        jumping = false
+        velocity += get_gravity() * delta
+      else:
+        if not landed:
+          $frog_hop_audio._play_landing(false)
+          landed = true
+      
+        velocity.x *= damping_factor
+        if abs(velocity.x) < 0.1:
+          velocity.x = 0
+    PlayerState.water:
+      pass
+    PlayerState.breached:
+      pass
   move_and_slide()
   
 func make_trail():
