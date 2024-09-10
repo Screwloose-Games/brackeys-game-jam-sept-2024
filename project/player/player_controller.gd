2df53aa07325a3f03d32291962b1e5f35730e7b7
Simpathey <62914757+Simpathey@users.noreply.github.com>
2024-09-09 19:44:42 -0700

    Water Working
    Player can swim, breach the surface and hop out of the water

--- project/player/player_controller.gd
+++ project/player/player_controller.gd
@@ -4,8 +4,8 @@ enum PlayerState {land,water,breached,}
 @export  var base_jump_velocity = -10.0
 @export var damping_factor = 0.6
 @export var water_damping_factor = 0.8
-@export var breach_threshold = 5 
-@export var water_breach_jump_boost = 1.1
+@export var breach_position_surface_offset = 3 
+@export var water_breach_jump_boost = 2
 @export var trail_offset: Node2D
 
 @onready var jump_arrow: JumpArrow = %JumpArrow
@@ -14,10 +14,15 @@ enum PlayerState {land,water,breached,}
 var jumping = false
 var current_trail: JumpTrail
 var landed = false
-@export var player_state: PlayerState = PlayerState.land
+var jump_from_breach: bool = false
+var water_layer: int = 4
+var breach_layer: int = 8
+@export var player_state: PlayerState = PlayerState.water
+
 
 func _ready():
   jump_arrow.jump.connect(_on_jump)
+  make_trail()
 
 func _on_jump(angle: float, power: float):
   match player_state:
@@ -32,7 +37,6 @@ func _on_jump(angle: float, power: float):
         velocity.y = v_y
         jumping = true
         $frog_hop_audio._play_hop()
-        make_trail()
     PlayerState.water:
       var direction = Vector2(cos(angle), sin(angle))
       
@@ -42,9 +46,11 @@ func _on_jump(angle: float, power: float):
       var v_y = direction.y * power * base_jump_velocity
       velocity.y = v_y
       jumping = true
+      #play swim sound here
       $frog_hop_audio._play_hop()
       
     PlayerState.breached:
+      jump_from_breach = true
       var direction = Vector2(cos(angle), sin(angle))
       
       var v_x = direction.x * power * base_jump_velocity * water_breach_jump_boost
@@ -53,7 +59,7 @@ func _on_jump(angle: float, power: float):
       var v_y = direction.y * power * base_jump_velocity * water_breach_jump_boost
       velocity.y = v_y
       jumping = true
-      pass
+      
         
 func _physics_process(delta: float) -> void:
   match player_state:
@@ -78,8 +84,9 @@ func _physics_process(delta: float) -> void:
         velocity.y = 0
         
     PlayerState.breached:
-      var new_pos = position.move_toward(Vector2(position.x,water_ref.current_water_height),5)
-      position = new_pos
+      velocity.x *= water_damping_factor
+      if not jump_from_breach:
+        position = Vector2(position.x,-water_ref.current_water_height-breach_position_surface_offset)
   move_and_slide()
   
 func make_trail():
@@ -94,17 +101,26 @@ func make_trail():
 
 func _on_water_detector_area_entered(area:Area2D):
   if player_state == PlayerState.water:
-    if area.collision_layer == 8:
+    if area.collision_layer == breach_layer:
       player_state = PlayerState.breached
+      velocity = Vector2.ZERO
+      #print("entering breached - play breaching water sound here")
   if player_state == PlayerState.land or PlayerState.breached:
-    if area.collision_layer == 4:
+    if area.collision_layer == water_layer:
+      #print("entered water - play landing in water sound")
+      jump_from_breach = false
       player_state = PlayerState.water
   
-  
-
 func _on_water_detector_area_exited(area):
   if player_state == PlayerState.breached:
-    if area.collision_layer == 8:
+    if area.collision_layer == breach_layer:
       player_state = PlayerState.land
+      jump_from_breach = false
+      #print("on land - play jumping out of water sound")
+  if player_state == PlayerState.water:
+    if area.collision_layer == water_layer:
+      player_state = PlayerState.breached
+      velocity = Vector2.ZERO
+      #print("entering breached - play breaching water sound here")
+      
     
-  player_state = PlayerState.land
