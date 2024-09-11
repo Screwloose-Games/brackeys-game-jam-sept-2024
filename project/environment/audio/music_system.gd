8631e59ff08f4ec64b46ad564dfa4282a8e60488
ChristophGray <chrisgraydev@gmail.com>
2024-09-11 07:52:30 +0100

    volume bus control, low pass filter and music transitions
    Set up some functions for the volume sliders, just need to call _set_volume() and pass in the float val and the bus type. You can also initialize your sliders with the _get_volume_normalized() function.
    
    In terms of music, you just need to call _change_music and pass in one of the audiostream variables and the transition time in seconds.
    
    With the water toggle you need to call _toggle_water_effect() and pass in a bool.

---
+++ project/environment/audio/music_system.gd
@@ -0,0 +1,134 @@
+extends Node
+
+enum audio_bus_type { MASTER, MUSIC, SFX }
+enum track_type { TRACK_A, TRACK_B }
+
+@export_range(0.0, 1.0) var master_vol : float
+@export_range(0.0, 1.0) var sfx_vol : float
+@export_range(0.0, 1.0) var music_vol : float
+
+@export var filter_transition_time : float
+@export_range(0.0, 20000) var cutoff : float
+@export_range(0.0, 1.0) var resonance : float
+
+# Series of music tracks that can be referenced when changing the music
+@export var theme : AudioStreamMP3
+@export var level_light : AudioStreamMP3
+
+# Audio players
+@onready var track_a = $track_A
+@onready var track_b = $track_B
+
+var current_track = track_type.TRACK_A
+
+#filter
+var lps_filter : AudioEffectFilter
+
+# Called when the node enters the scene tree for the first time.
+# init bus volumes and effects and play theme music
+func _ready() -> void:
+  _set_volume(audio_bus_type.MASTER, master_vol)
+  _set_volume(audio_bus_type.SFX, sfx_vol)
+  _set_volume(audio_bus_type.MUSIC, music_vol)
+  
+  _init_filter()
+  _init_music(theme)
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  print("track A vol: " + str(track_a.volume_db))
+  print("track B vol: " + str(track_b.volume_db))
+  pass
+  
+func _get_volume_normalized(audio_type : audio_bus_type) -> float:
+  match(audio_type):
+    audio_bus_type.MASTER:
+      return db_to_linear(AudioServer.get_bus_volume_db(0))
+    audio_bus_type.SFX:
+      return db_to_linear(AudioServer.get_bus_volume_db(1))
+    audio_bus_type.MUSIC:
+      return db_to_linear(AudioServer.get_bus_volume_db(2))
+      
+  return db_to_linear(AudioServer.get_bus_volume_db(0))
+  
+func _get_volume(audio_type : audio_bus_type) -> float:
+  match(audio_type):
+    audio_bus_type.MASTER:
+      return AudioServer.get_bus_volume_db(0)
+    audio_bus_type.SFX:
+      return AudioServer.get_bus_volume_db(1)
+    audio_bus_type.MUSIC:
+      return AudioServer.get_bus_volume_db(2)
+      
+  return AudioServer.get_bus_volume_db(0)
+
+func _set_volume(audio_type : audio_bus_type, value : float) -> void:
+  match(audio_type):
+    audio_bus_type.MASTER:
+      master_vol = value
+      AudioServer.set_bus_volume_db(0, linear_to_db(value))
+    audio_bus_type.SFX:
+      sfx_vol = value
+      AudioServer.set_bus_volume_db(1, linear_to_db(value))
+    audio_bus_type.MUSIC:
+      music_vol = value
+      AudioServer.set_bus_volume_db(2, linear_to_db(value))
+        
+  pass
+  
+# Play starting music - probably the theme
+func _init_music(new_track : AudioStreamMP3) -> void:
+  track_b.volume_db = -80.0
+  track_a.volume_db = 0.0
+  track_a.stream = new_track
+  track_a.play()
+  
+  pass
+  
+func _init_filter() -> void:
+  lps_filter = AudioServer.get_bus_effect(0, 0)
+  lps_filter.cutoff_hz = 20500
+  lps_filter.resonance = 0.5
+  lps_filter.db = AudioEffectFilter.FILTER_24DB
+  pass
+  
+func _change_music(new_track : AudioStreamMP3, transition_time : float) -> void:
+  var last_track = current_track
+  match (current_track):
+    track_type.TRACK_A:
+      last_track = track_type.TRACK_B
+      if (track_b.stream != new_track):
+        track_b.stream = new_track
+        track_b.play()
+        
+      #tween other track to stop using transition time and stop playing after transition time
+      var tween = get_tree().create_tween()
+      tween.tween_property(track_b, ":volume_db", linear_to_db(1.0), transition_time)
+      tween.tween_property(track_a, ":volume_db", linear_to_db(0.0001), transition_time)
+      
+    track_type.TRACK_B:
+      last_track = track_type.TRACK_A
+      if (track_a.stream != new_track):
+        track_a.stream = new_track
+        track_a.play()
+        
+      #tween other track to stop using transition time and stop playing after transition time
+      var tween = get_tree().create_tween()
+      tween.tween_property(track_a, ":volume_db", linear_to_db(1.0), transition_time)
+      tween.tween_property(track_b, ":volume_db", linear_to_db(0.0001), transition_time)
+    
+  current_track = last_track
+  pass
+  
+func _toggle_water_effect(is_under_water : bool) -> void:
+  if (is_under_water):
+    var tween = get_tree().create_tween()
+    tween.tween_property(lps_filter, ":cutoff_hz", cutoff, filter_transition_time)
+    tween.tween_property(lps_filter, ":resonance", resonance, filter_transition_time)
+  else:
+    var tween = get_tree().create_tween()
+    tween.tween_property(lps_filter, ":cutoff_hz", 20500, filter_transition_time)
+    tween.tween_property(lps_filter, ":resonance", 0.5, filter_transition_time)
+  pass
