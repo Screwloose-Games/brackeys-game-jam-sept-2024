00ea289fcf03e2cba07bd349c0fa801a8a9bd388
ChristophGray <chrisgraydev@gmail.com>
2024-09-12 09:31:47 +0100

    scene change from main menu with fade transition

--- project/ui/main_menu.gd
+++ project/ui/main_menu.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 @onready var start_button = $start_button
 
-var level_scene = preload("res://world/level_01.tscn").instantiate()
+var level_scene = preload("res://world/level_01.tscn")
+var fade_transition = preload("res://ui/scene_transitions/fade_transition.tscn")
 var audio_control = AudioControl
+var scene_transition = SceneTransitionManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,8 +18,5 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-  get_tree().root.add_child(level_scene)
-  var main_level = get_tree().current_scene
-  get_tree().current_scene = level_scene
-  get_tree().root.remove_child(main_level)
+  scene_transition.change_scene_with_transition(level_scene, fade_transition)
   audio_control._change_music(audio_control.level_light, 1.5)
