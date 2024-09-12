0ba2fabfae5f562350733e473e69ff5071235eae
ChristophGray <chrisgraydev@gmail.com>
2024-09-12 07:20:15 +0100

    removed hover sound variable

--- project/environment/audio/ui_audio.gd
+++ project/environment/audio/ui_audio.gd
@@ -1,13 +1,7 @@
 extends Node
 
-@export var hover_sound : AudioStreamWAV
-
 @onready var hover = $hover_audio
 @onready var confirm = $confirm_audio
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  hover.stream = hover_sound
 
 func _hover() -> void:
   hover.play()
