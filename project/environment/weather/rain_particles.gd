e866561d4344bb5807204419fa19406ca5476034
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-09-14 11:13:00 -0700

    Configure rain visual for compatability

--- project/environment/weather/rain_particles.gd
+++ project/environment/weather/rain_particles.gd
@@ -10,17 +10,27 @@ func _ready() -> void:
   var tween = get_tree().create_tween()
   start_heavy_rain()
 
+# Play rain and wind effects from 0.0 - 1.0 in stength
+# calculate the effect of rain intensity and wind intensity on the final rain effect
+func start_weather(rain_amount: float, wind_amount: float):
+  pass
+
 func start_light_rain():
   var material: ParticleProcessMaterial = process_material
-  amount = 200
+  amount = 400
   material.direction = Vector3(0, 1, 0)
-  material.initial_velocity_min = 800
-  material.initial_velocity_max = 1000
+  material.initial_velocity_min = 1200
+  material.initial_velocity_max = 2000
+  material.angle_min = 0
+  material.angle_max = 0
   var text: GradientTexture2D = texture
   text.width = 1
+  text.height = 10
   lifetime = 0.4
-  trail_lifetime = 0.06
   weather_sfx._change_rain(false)
+
+func start_rain(amount, direction, vel_min, vel_max, angle_min, angle_max, texture_w, texture_h, lifetime):
+  pass
 
 func start_heavy_rain():
   var material: ParticleProcessMaterial = process_material
@@ -28,10 +38,12 @@ func start_heavy_rain():
   material.direction = Vector3(1.0, 0.5, 0)
   material.initial_velocity_min = 800
   material.initial_velocity_max = 4000
+  material.angle_min = 60
+  material.angle_max = 60
   var text: GradientTexture2D = texture
   text.width = 2
+  text.height = 64
   lifetime = 0.4
-  trail_lifetime = 0.06
   weather_sfx._change_rain(true)
 
 func _process(delta: float) -> void:
